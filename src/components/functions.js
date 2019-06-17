6d9369344d0a8940b030f1f0b78e04172d2ce339
layshidani <lays.hidani@gmail.com>
2019-06-17 16:52:26 -0300

    Remove unused files

--- src/components/functions.js
+++
@@ -1,9 +0,0 @@
-// import firebase from '../firebase/firebase-config';
-// const firebaseAppAuth = firebase.auth();
-
-// export const signOut = () => {
-//   console.log('clicou');
-  
-//   firebaseAppAuth.signOut()
-//   this.props.history.push(`/`)
-// }
